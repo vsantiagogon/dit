ec4fd8782cbcd814af8fdb1eda37ae0d676abdc1
victor <viktorsantiagogonzalez@gmail.com>
2018-02-04 11:24:15 +0000

    [DSP] Lab2 inheritance & plots

--- dsp/lab2.py
+++ dsp/lab2.py
@@ -1,19 +1,42 @@
 import numpy as np 
 import librosa as lsa
-import sounddevice as sd 
+import matplotlib.pyplot as plt
+import librosa.display as lsd
+import sounddevice as sd
+import platform
+import os
 
 
-class Beep:
-
+# Defines utility functions for Sounds: plot, save & play
+class Sound: 
 	SR = 44100
 
-	def __init__(self, time, freq1, freq2):
-		t = np.arange(0, time, 1 /self.SR )
-		self.wave = np.cos(np.pi * t * (freq1 + freq2)) * np.cos(np.pi * t * (freq1 - freq2))
+	def play(self):
+		sd.play(self.wave, blocking = True)
 
 	def play (self):
 		sd.play(self.wave, blocking = True)
 
 	def save (self, name):
-		print(len(self.wave))
 		lsa.output.write_wav(name, self.wave, self.SR, True)
+
+	def plot(self):
+		plt.figure()
+		lsd.waveplot(self.wave, self.SR)
+		plt.title('pruebas')
+		plt.show()
+
+# Tones are defined as x(t) = 0.5 cos(2*pi*freq*t)
+class Signal(Sound): 
+
+	def __init__(self, time, freq):
+		t = np.arange(0, time, 1 / self.SR)
+		self.wave = 0.5 * np.cos(2 * np.pi * freq * t)
+
+# DTMF tones are sums of signals. But cos(A + B) = 2 cos(A + B)cos(A-B) so, now
+# 	x(t) := cos(pi * t * (f1 + f2)) * cos(pi * t * (f1 - f2))
+class Beep(Sound):
+
+	def __init__(self, time, freq1, freq2):
+		t = np.arange(0, time, 1 /self.SR )
+		self.wave = np.cos(np.pi * t * (freq1 + freq2)) * np.cos(np.pi * t * (freq1 - freq2))
