c7bc763aec98e80eecb782456ec44a0db5217958
victor <viktorsantiagogonzalez@gmail.com>
2018-02-07 22:59:30 +0000

    [DSP] Lab2 loading sounds

--- dsp/lab2.py
+++ dsp/lab2.py
@@ -2,10 +2,15 @@ import numpy as np
 import librosa as lsa
 import matplotlib.pyplot as plt
 import sounddevice as sd
+from scipy import signal
 
 # Defines utility functions for Sounds: plot, save & play
 class Sound: 
 	SR = 44100
+
+	def __init__(self, duration, wave):
+		self.time = np.arange(0, duration, 1 /self.SR)
+		self.wave = wave
 
 	def play(self):
 		sd.play(self.wave, blocking = True)
@@ -22,6 +27,21 @@ class Sound:
 		plt.xlabel('time(s)')
 		plt.title(title)
 		plt.show(block=False)
+	
+	def filter(self, order = 6, cutoff = 1000):
+		cutoff = cutoff / (0.5 * self.SR)
+		a, b = signal.butter(order, cutoff)
+		y = signal.filtfilt(b, a, self.wave)
+		plt.figure()
+		plt.plot(self.time, self.wave, alpha=0.75)
+		plt.plot(self.time, y, color='grey')
+		plt.show(block=False)
+
+# Create a Sound from WAV file
+def load(name):
+	wave, SR = lsa.core.load(name, sr = 44100)
+	duration = lsa.core.get_duration(y = wave, sr = SR)
+	return Sound(duration, wave)
 
 # Tones are defined as x(t) = 0.5 cos(2*pi*freq*t)
 class Signal(Sound): 
