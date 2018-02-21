bdb2229157bfdd35feb37dd10b1e6fbbc201dcd3
victor <viktorsantiagogonzalez@gmail.com>
2018-02-21 17:40:10 +0000

    [ML] R-lab. KNN applied successfully

--- ml/Rlab.R
+++ ml/Rlab.R
@@ -1,22 +1,12 @@
-if (!require('dataQualityR')) {
-  install.packages('dataQualityR');
+packages = function (pkgs) {
+  for (pkg in pkgs) {
+    if (!(pkg %in% installed.packages())) install.packages(pkg);
+    library(pkg, character.only = T);
+  } 
 }
-library(dataQualityR);
 
-if (!require('class')) {
-  install.packages('class');
-}
-library(class);
+packages(c('dataQualityR','class', 'dplyr', 'ggplot2')) #gmodels
 
-if (!require('gmodels')) {
-  install.packages('gmodels')
-}
-library(gmodels)
-
-if (!require('dplyr')) {
-  install.packages('dplyr')
-}
-library(dplyr)
 
 data = read.csv('repos/dit/ml/train.csv', stringsAsFactors = FALSE, na.strings = 'NA')
 
@@ -34,7 +24,7 @@ getDataQuality <- function (df) {
   file.remove(paste(tempdir(), '/dq_cat.csv', sep= ''))
   
   return (list(
-    'Continuous' = DQ.numerical,
+    'Numerical' = DQ.numerical,
     'Categorical' = DQ.categorical
   ))
 }
@@ -44,13 +34,13 @@ getDataQuality <- function (df) {
 ### REPORT INITIAL DATA QUALITY
 
 DQ = getDataQuality(data);
-View(DQ$Continuous);
+View(DQ$Numerical);
 View(DQ$Categorical);
 
 ### DATA PREPARATION
 
 # Obvious drops
-data = subset(data, select = -c(PassengerId, Name, Ticket))
+data = subset(data, select = -c(Name, Ticket))
 
 # Cabin has a ~77% of missing values. We drop it as well.
 
@@ -65,6 +55,31 @@ data[is.na(data$Age) == TRUE, ]$Age = median(data$Age, na.rm = TRUE)
 
 # Check data quality again.
 DQ = getDataQuality(data);
-
-View(DQ$Continuous)
+View(DQ$Numerical)
 View(DQ$Categorical)
+
+### DATA VISUALIZATION. 
+
+## Continuous variables. 
+
+ggplot(data, aes(x = Age, y = Fare)) + geom_point() + theme_classic() + ggtitle('Fare vs Age')
+
+### KNN
+
+# The algorithm can only deal with numbers.
+
+data$Sex = sapply(as.character(data$Sex), switch, 'male' = 0, 'female' = 1);
+data$Embarked = sapply(as.character(data$Embarked), switch, 'C' = 0, 'Q' = 1, 'S' = 2);
+
+# Randomize the data
+data = data[sample(nrow(data)), ]
+
+train = head(data, round(0.7 * nrow(data)));
+test  = tail(data, nrow(data) - round(0.7 * nrow(data)));
+
+predictions <- data.frame(
+  'ID' = test$PassengerId,
+  'Survived' = knn(select(train, -c(PassengerId)), select(test, -c(PassengerId)), train$Survived, k=10)
+) %>% arrange(ID)
+
+View(predictions)
