7f557a13dc32dc1b9230b649e0d966d41187a8ff
victor <viktorsantiagogonzalez@gmail.com>
2018-03-14 19:20:22 +0000

    [ML] RLab3 NA imputed

--- ml/RLab3/main.R
+++ ml/RLab3/main.R
@@ -3,3 +3,26 @@ ROOT = 'repos/dit/ml/'
 source(paste(ROOT, 'utils/Setup.R', sep = ''));
 
 Setup$getPkgs(c('C50', 'dataQualityR', 'dplyr', 'ggplot2'));
+
+DATA = read.csv(paste(ROOT, '../datasets/lab3.csv', sep = ''), stringsAsFactors = FALSE, na.string="?");
+
+source( paste( ROOT, 'RLab2/quality.R', sep = '' ) );
+
+# DATA QUALITY REPORT.
+#  Report <- DQ(DATA);
+#  View(Report$Cat);
+
+# DATA PREPARATION.
+  
+# NORMALIZED.LOSSESS miss ~20% -> Impute by mean
+
+# Age contains a 22% of NA's. We impute them using the median.
+DATA[is.na(DATA$normalized.losses) == T, ]$normalized.losses = mean(as.numeric(DATA$normalized.losses), na.rm = TRUE);
+    
+# REJECT ROWS IN COLUMNS WITH VERY FEW NA's miss ~2% -> reject NA's.
+DATA = filter(DATA, !is.na(bore) & !is.na(num.of.doors) & !is.na(num.of.doors) & !is.na(stroke) & !is.na(horsepower) & !is.na(peak.rpm) & !is.na(price));
+  
+# DATA QUALITY REPORT.
+Report <- DQ(DATA);
+View(Report$Cat);
+
