545c4340eff94e88fa383b4809b9da52f9f5651f
victor <viktorsantiagogonzalez@gmail.com>
2018-03-21 15:02:38 +0000

    [ML] RLab4 corpus & wordcloud

--- ml/RLab4/main.R
+++ ml/RLab4/main.R
@@ -2,11 +2,17 @@ ROOT = paste(getwd(), '/repos/dit/', sep = '');
 
 source(paste(ROOT, 'ml/utils/Setup.R', sep=''));
 
-Setup$getPkgs('dplyr');
+Setup$getPkgs(c('dplyr', 'tm'));
 
-DATA <- read.csv(
-      paste(ROOT, 'datasets/SMSSpanCollection.txt', sep='')
-    , stringsAsFactors = FALSE, sep=",", quote="\'"
-  ) %>% select(1:2);
+DATA <- read.csv(paste(ROOT, 'datasets/SMSSpanCollection.txt', sep=''), stringsAsFactors = F, sep=",", quote="\'") %>% 
+  select(1:2);
 
 colnames(DATA) <- c('text', 'label');
+
+# Create and clean corpus.
+corpus = Corpus(VectorSource(DATA$text)) %>%  # CREATION
+  tm_map(tolower) %>%                         # LOWERCASE
+  tm_map(removeNumbers) %>%                   # NO NUMBERS ALLOWED
+  tm_map(removePunctuation) %>%               # REMOVE PUNCTUATION
+  tm_map(stripWhitespace);                    # TRIM WHITESPACES
+
