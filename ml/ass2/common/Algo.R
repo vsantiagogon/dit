3044b92c4bf66d73668724b2843172f7b71289dd
Victor <viktorsantiagogonzalez@gmail.com>
2018-04-19 11:19:22 +0100

    [ML-ASS2] adaboost to algol class

--- ml/ass2/common/Algo.R
+++ ml/ass2/common/Algo.R
@@ -18,6 +18,40 @@ Algo = (function (path) {
       stopCluster(cl);
       
       return (rowMeans(predictions));
+    },
+    
+    adaboost = function (train, test, M, clf) {
+      n = nrow(train);
+      
+      w = rep(1/n, n);
+      
+      train$spam = ifelse(train$spam == 0, -1, 1);
+      test$spam  = ifelse(test$spam  == 0, -1, 1);
+      
+      output = rep(0, nrow(test)); # General model is a sum. We use neutral 0 to initialize
+      
+      for (i in 1:M) {
+        #model      = clf(spam ~ ., data = train, weights = w);
+        classifier = clf(train, ada = TRUE, weights = w);
+        # We want to modify weights on those points where we find difficulties to predict. 
+        # That's why we do this silly thing of calculate predictions over the train dataset.
+        preds = classifier(train);
+        preds_test = classifier(test);
+        #preds      = ifelse(predict(model, train[, labels]) > 0, 1, -1 );
+        #preds_test = ifelse(predict(model, test[, labels]) > 0, 1, -1 );
+        
+        ind = data.frame(actual = train$spam, preds = preds) %>% mutate(ind = ifelse(actual != preds, 1, 0)) %>% select(ind);
+        
+        err = sum(w %*% ind[ , 1 ]) / sum(w);
+        
+        alpha = 0.5 * log( (1 - err )/ err );
+        
+        w = w * exp(alpha * ind[, 1]);
+        
+        output = output + c(alpha * preds_test);
+      }
+      
+      return ( sign(output) );
     }
     
   ))
